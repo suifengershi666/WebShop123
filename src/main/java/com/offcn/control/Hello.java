9b0adf698df3ec09b8a2b29aa67eb60eda2c40e3
suifengershi666 <dingguanghui666@sohu.com>
2020-04-06 20:44:48 +0800

    增加Hello.java

---
+++ src/main/java/com/offcn/control/Hello.java
@@ -0,0 +1,11 @@
+package com.offcn.control;
+
+/**
+ * @author dahuige
+ * @date 2020/4/6-20:42
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
